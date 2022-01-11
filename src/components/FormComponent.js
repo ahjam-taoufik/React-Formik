0070aa4d29a07541885820983e05d3b302438a4b
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-11 20:57:10 +0100

    14-Field-Level-Validation

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,61 +1,37 @@
-import { Formik, Form, Field, ErrorMessage, FieldArray, FastField } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from "formik";
 import React from "react";
 import * as Yup from "yup";
 import TextError from "./TextError";
 const FormComponent = () => {
   const initialValues = {
     name: "",
-    numbrPhones: ['']
   };
   const onSubmit = (values) => {
     console.log("onsubmit :", values);
   };
 
-  const validationSchema = Yup.object({
-    name: Yup.string().required("Required name..."),
-  });
+  const errorName=value=>{
+    let error=''
+     
+     if (!value) {
+         error='required'
+     }
+    return error
+  }
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-      validateOnBlur={false}
-      validateOnChange={false}
-    >
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
       <Form>
-
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <Field type="text" id="name" name="name" />
+          <Field type="text" id="name" name="name" validate={errorName} />
+
           <ErrorMessage name="name" component={TextError} />
-        </div>
-
-
-
-        <div className="form-control">
-          <label>List phones</label>
-          <FieldArray name="numbrPhones">
-            {FieldArrayProps => {
-              const { push, remove,form } = FieldArrayProps;
-              const { values } = form;
-                      console.log(form.errors);
-              const { numbrPhones } = values;
-
-              return (
-                <div>
-                  {numbrPhones.map((number, index) => (
-                    <div key={index}>
-                    
-                      <Field name={`numbrPhones[${index}]`} />
-                      {index>0 && (<button type="button" onClick={()=>remove(index)} >-</button>) }
-                      <button type="button" onClick={()=>push('')} >{'  '} +{" "}</button>
-                    </div>
-                  ))}
-                </div>
-              ); //return
-            }}
-          </FieldArray>
         </div>
 
         <button type="submit">Submit</button>
