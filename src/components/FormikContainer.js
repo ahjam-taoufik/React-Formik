d25d4d5317f6ae011adce8d27617f080f175e114
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-12 00:34:37 +0100

    select

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -4,13 +4,25 @@ import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 
 function FormikContainer() {
+
+const selectList=[
+    {key:"Select on option", value:''},
+    {key:"option 1", value:'option1'},
+    {key:"option 2", value:'option2'},
+    {key:"option 3", value:'option3'},
+
+]
+   
+
   const initialValues = {
     email: '',
-    comment: ''
+    comment: '',
+    selectOption:''
   };
   const validationSchema = Yup.object({
     email: Yup.string().email().required(),
     comment: Yup.string().required(),
+    selectOption: Yup.string().required()
   });
 
   const onSubmit = values =>{
@@ -37,6 +49,12 @@ function FormikContainer() {
             control="textarea"
             label="Comment"
             name="comment"
+          />
+          <FormikControl
+            control="select"
+            label="Select An Option"
+            name="selectOption"
+            options={selectList}
           />
           <button type='submit'>Submit</button>
         </Form>
--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Input from './Input'
 import TextArea from './TextArea'
+import Select from './Select'
 
 function FormikControl(props) {
     const {control, ...rest}=props
@@ -9,7 +10,9 @@ function FormikControl(props) {
             return <Input {...rest} />
         case'textarea':
             return   <TextArea  {...rest} />
-        case'select':
+            case'select':
+            return   <Select  {...rest} />
+
         case'radio':
         case'checkbox':
         case'date':
