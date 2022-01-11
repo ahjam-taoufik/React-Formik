2933c43a3183fd881a123d8c8589eb9f83baf08e
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-11 11:55:53 +0100

    Handling form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FormComponent from "./components/FormComponent";
+
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent/>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormComponent.js
@@ -0,0 +1,34 @@
+
+import { useFormik } from 'formik'
+import React from 'react'
+ 
+const FormComponent = () => {
+   
+const formik= useFormik({
+    initialValues:{
+        name:'',
+        email:'',
+        channel:''
+    }
+})
+
+console.log(formik.values);
+
+    return (
+        <div>
+          <form>
+             <label htmlFor='name'>Name</label>
+             <input onChange={formik.handleChange} value={formik.values.name}   type='text' id='name' name='name'/>
+
+             <label htmlFor='email'>Email</label>
+             <input onChange={formik.handleChange} value={formik.values.email}  type='email' id='email' name='email'/>
+             <label htmlFor='channel'>Channel</label>
+             <input onChange={formik.handleChange} value={formik.values.channel}  type='text' id='channel' name='channel'/>
+              <button type='submit'>Submit</button>
+          </form>
+
+        </div>
+    )
+}
+
+export default FormComponent
