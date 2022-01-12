fbfc62a13c6a53b042ec5f866cfbd71f90423f35
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-12 11:31:51 +0100

    Enrollment Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import RegistrationForm from "./components/RegistrationForm";
+import EnrollmentForm from "./components/EnrollmentForm";
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+  
+      <EnrollmentForm/>
     </div>
   );
 }
