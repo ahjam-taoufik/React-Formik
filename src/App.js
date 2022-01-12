5cc996f2ad4aa6c4b622527fe4c97803e9914729
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-12 11:11:18 +0100

    RegistrationForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import FormikContainer from "./components/FormikContainer";
-import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
-  
+      <RegistrationForm />
     </div>
   );
 }
